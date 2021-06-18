b1c5d4f43b9ad7ce3515d06a0d246c328df5ce39
Nick <degroona@mail.uc.edu>
2021-06-17 23:15:30 -0400

    Clean up distance calculation

--- Project2/fitness_functions/total_distance.py
+++ Project2/fitness_functions/total_distance.py
@@ -4,10 +4,8 @@ from typing import List
 
 def total_distance(route: List[int], dist_matrix: np.ndarray) -> float:
     total_dist = 0
-    previous_city = route[0]
-    for city in route[1:]:
-        total_dist += dist_matrix[previous_city, city]
-        previous_city = city
-    total_dist += dist_matrix[previous_city, route[0]]
+    for city, next_city in zip(route, route[1:]):
+        total_dist += dist_matrix[city, next_city]
+    total_dist += dist_matrix[route[-1], route[0]]
     fitness = 1 / total_dist
     return fitness
